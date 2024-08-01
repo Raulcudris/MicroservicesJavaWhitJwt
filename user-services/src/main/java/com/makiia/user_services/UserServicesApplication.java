b7ecc503244fc3169f781a3259ba31b409a5dfd7
RaulCudris <raulcudriz@hotmail.com>
2024-08-01 18:59:28 -0500

    Creacion del microservicios userServices

---
+++ user-services/src/main/java/com/makiia/user_services/UserServicesApplication.java
@@ -0,0 +1,14 @@
+package com.makiia.user_services;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class UserServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServicesApplication.class, args);
+	}
+
+}
